50dfe85ee5938760875903ecf9045822d0265c94
whitesad <whitesad_01@outlook.com>
2021-11-14 21:43:23 +0800

    添加了对于daily setu模块的info提示

--- christinaqqbot/utils/time.py
+++ christinaqqbot/utils/time.py
@@ -2,7 +2,7 @@ import datetime,requests
 
 def get_beijing_time():
     try:
-        intime = str(requests.get("http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp").text)
+        intime = str(requests.get("http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp",timeout=2).text)
         one = intime[:intime.rfind('"')]
         times = datetime.datetime.fromtimestamp(int(one[one.rfind('"')+1:-3]))
     except Exception:
